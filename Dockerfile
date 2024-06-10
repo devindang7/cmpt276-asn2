27b142a3aa6676cb960852af5978664d3108272f
Devin <devindang7@gmail.com>
2024-06-10 16:18:59 -0700

    Dockerfile, Rectangle model

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+# compile application
+FROM maven:3-openjdk-17 AS build
+COPY . .
+RUN mvn clean package -DskipTests
+
+# run application
+FROM openjdk:17.0.1-jdk-slim
+COPY --from=build /target/asn2-0.0.1-SNAPSHOT.jar asn2.jar
+EXPOSE 8080
+ENTRYPOINT ["java","-jar","asn2.jar"]
--- src/main/java/cmpt276/asn2/models/Rectangle.java
+++ src/main/java/cmpt276/asn2/models/Rectangle.java
@@ -3,7 +3,54 @@ package cmpt276.asn2.models;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "Rectangle")
+@Table(name="Rectangle")
 public class Rectangle {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int uid;
+    private String name;
+    private int width;
+    private int height;
+    private String color;
+    
+    public Rectangle() {
+    }
+    public Rectangle(int uid, String name, int width, int height, String color) {
+        this.uid = uid;
+        this.name = name;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+    public int getUid() {
+        return uid;
+    }
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getWidth() {
+        return width;
+    }
+    public void setWidth(int width) {
+        this.width = width;
+    }
+    public int getHeight() {
+        return height;
+    }
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
     
 }
