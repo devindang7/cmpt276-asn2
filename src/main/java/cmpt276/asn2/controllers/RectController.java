9a11d37a5305aca3dc392cb5b10c9aa2390156a3
Devin <devindang7@gmail.com>
2024-06-14 22:21:53 -0700

    finished

--- src/main/java/cmpt276/asn2/controllers/RectController.java
+++ src/main/java/cmpt276/asn2/controllers/RectController.java
@@ -23,17 +23,19 @@ public class RectController {
     private RectRepository rectRepo;
     @GetMapping("/rectangles")
     public String getRectangles(Model model) {
-        List<Rectangle> rects = rectRepo.findAll(Sort.by(Sort.Direction.ASC, "uid"));
-        model.addAttribute("rects", rects);
+        List<Rectangle> rects = rectRepo.findAll(Sort.by(Sort.Direction.ASC, "uid")); // sorted to fix order after delete
+        model.addAttribute("rects", rects); 
         return "allRects.html";
     }
 
     @PostMapping("/rectangles/add")
     public String addRect(@RequestParam Map<String, String> newRect, HttpServletResponse response){
+        // get all needed values from the form
         String newName = newRect.get("name");
         int newWidth = Integer.parseInt(newRect.get("width"));
         int newHeight = Integer.parseInt(newRect.get("height"));
         String newColor = newRect.get("color");
+        // save the new rectangle to the database
         rectRepo.save(new Rectangle(newName,newWidth,newHeight,newColor));
         response.setStatus(201);
         return "redirect:/rectangles";
@@ -48,15 +50,18 @@ public class RectController {
 
     @PostMapping("/rectangles/edit/{id}")
     public String editRect(@RequestParam Map<String, String> rect, @PathVariable int id, HttpServletResponse response){
+        // get all new values or current values from form
         String newName = rect.get("name");
         int newWidth = Integer.parseInt(rect.get("width"));
         int newHeight = Integer.parseInt(rect.get("height"));
         String newColor = rect.get("color");
+        // update the rectangle in the database
         Rectangle currentRect = rectRepo.findById(id).get();
         currentRect.setName(newName);
         currentRect.setWidth(newWidth);
         currentRect.setHeight(newHeight);
         currentRect.setColor(newColor);
+        // save the updated rectangle
         rectRepo.save(currentRect);
         response.setStatus(201);
         return "redirect:/rectangles";
@@ -64,6 +69,7 @@ public class RectController {
 
     @DeleteMapping("/rectangles/delete/{id}")
     public String deleteRect(@PathVariable int id){
+        // delete rectangle from database using uid
         Rectangle currentRect = rectRepo.findById(id).get();
         rectRepo.delete(currentRect);
         return "redirect:/rectangles";
